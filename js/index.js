8e544f01f6c0b76bdc84ebe3fb44456f5a3cf094
dhirajsharmadk <98511332+dhirajsharmadk@users.noreply.github.com>
2023-02-21 21:19:27 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// let age = prompt("Enter Your Age") 
+// age = Number.parseInt(age)
+
+// const canLogin = (age)=>{
+//  return age>=10?true:false
+// }
+// if(canLogin(age)){
+//       alert("Yes You Can Login")
+// }
+// else {
+//       alert("You Cannot Login")
+// }
